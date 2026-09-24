0d29f26cb6548ae22d663c17972393714427b2bf
Divyansh Kushwaha <divyanshh.ds@gmail.com>
2026-09-24 12:39:02 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='In this repo i am making a project on emotion detection using mlflow, dagshub, dvc pipeline, S3 storage.',
+    author='Divyansh Kushwaha',
+    license='MIT',
+)
